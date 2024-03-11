be7bb9605fbda746a20f3177886f6aeddd7b8ee2
qureshi96 <usmanramses@gmail.com>
2024-03-11 20:10:55 +0000

    env variable added

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var cors=require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.listen(3000, ()=>{
+const PORT=process.env.PORT ||3000;
+app.listen(PORT, ()=>{
     console.log('node running on port 3000')
 })
 
